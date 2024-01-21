5970bcb0a15098df91866889563079c66a44a6a0
Ankesh Kumar <ankesh9053@gmail.com>
2024-01-21 23:44:23 +0530

    done with today work done with the homepage basic thing is remainning

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -1,12 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
+import Homepage from './Components/Homepage';
 // import Api from './Practice/Api';
 import APiuse from './Practice/APiuse';
 // import {Api} from "./Practice/Api"
 function App() {
   return (
     <div className="App">
-      <APiuse/>
+      {/* <APiuse/> */}
+      <Homepage/>
     </div>
   );
 }
